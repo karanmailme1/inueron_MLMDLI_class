f81e38949a9ab713900ab1e691014e02a8ed8f4f
kv <karanmailme1@gmail.com>
2021-05-20 00:50:16 +0530

    just some indentation change in test.py

--- class work download/session7_Rest aip and sql and nosql/21.Flask/flask_demo/test.py
+++ class work download/session7_Rest aip and sql and nosql/21.Flask/flask_demo/test.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask  # ,render_template,request,jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/karan1', methods=['POST'])
 def test():
     return "test_this1"
-    #return jsonify(1+2)
-    #return 4 #error
+    # return jsonify(1+2)
+    # return 4 #error
 
-@app.route('/karan2') #default is GET method
+
+@app.route('/karan2')  # default is GET method
 def test2():
     return "test_this2"
-    #return jsonify(1+2)
-    #return 4 #error
+    # return jsonify(1+2)
+    # return 4 #error
+
 
 if __name__ == '__main__':
     app.run(debug=True)
